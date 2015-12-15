901b08415723bf850f5944777439c694589016bd
hariprakashmuvva <harimuvva150478@gmail.com>
2015-12-15 23:49:25 +0530

    plus and minus functionality

--- frontend/js/items.js
+++ frontend/js/items.js
@@ -8,27 +8,36 @@ $(document).ready(function(){
             $(this).addClass('active');
     });
 
-	var i = 0;
-    var livingRoomItems = [{"item1":"one", "item2":"two", "item3":"three", "item4":"four"}];
-    var bedRoomItems = [{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"}];    
-	var kitchenItems = [{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"}];    
-	var storeRoomItems = [{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"}];    
-	var poojaRoomItems = [{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"}];
-	var vehicleItems = [{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"}];    
-	var otherItems = [{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"},{"item1":"one", "item2":"two", "item3":"three", "item4":"four"}];
+	
+    var livingRoomItems = [{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"checked", "id":"104"}, {"itemName":"Cooler", "Qty":"", "Kg":"", "Size":"small", "Delicate":"", "id":"103"},{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"checked", "id":"101"}, {"itemName":"Cooler", "Qty":"", "Kg":"", "Size":"small", "Delicate":"", "id":"102"}];
+    var bedRoomItems = [{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"true", "id":"101"}, {"itemName":"Cooler", "Qty":"0", "Kg":"0", "Size":"small", "Delicate":"true", "id":"102"}];
+	var kitchenItems = [{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"true", "id":"101"}, {"itemName":"Cooler", "Qty":"0", "Kg":"0", "Size":"small", "Delicate":"true", "id":"102"}];
+	var storeRoomItems = [{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"true", "id":"101"}, {"itemName":"Cooler", "Qty":"0", "Kg":"0", "Size":"small", "Delicate":"true", "id":"102"}];
+	var poojaRoomItems = [{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"true", "id":"101"}, {"itemName":"Cooler", "Qty":"0", "Kg":"0", "Size":"small", "Delicate":"true", "id":"102"}];
+	var vehicleItems = [{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"true", "id":"101"}, {"itemName":"Cooler", "Qty":"0", "Kg":"0", "Size":"small", "Delicate":"true", "id":"102"}];
+	var otherItems = [{"itemName":"TV", "Qty":"", "Kg":"", "Size":"small", "Delicate":"true", "id":"101"}, {"itemName":"Cooler", "Qty":"0", "Kg":"0", "Size":"small", "Delicate":"true", "id":"102"}];
     function ItemsBoxes(data, container){
-    	$.each(data, function() {
-		  $.each(this, function(name, value) {	++i;
-		  	var itemBox = '<div class="col-xs-2 itemsboxes" id="itemsbox'+i+'"><div class="col-xs-12 wtSize"><div class="col-xs-4"><input type="text" class="form-control input-number" placeholder="Qty"></div><div class="col-xs-5"><input type="text" class="form-control" placeholder="Kg"></div><div class="col-xs-3"><i class="fa fa-laptop fa-3x" style="float:right"></i></div></div><div class="clearfix"></div><div class="col-xs-12"><div class="itemName">'+value+'</div></div><div class="clearfix"></div><div class="col-xs-12 wtSize"><div class="col-xs-6"><select class="form-control"><option>Small</option><option>Medium</option><option>Large</option><option>Ex Large</option></select></div><div class="col-xs-6"><div class="checkbox"><label><input type="checkbox"> Delicate</label></div></div></div><div class="clearfix"></div><div class="itemsboxBtns"><button id="btns" type="button" class="col-xs-4 btn btn-danger btn-number"  data-type="minus"><span class="glyphicon glyphicon-minus"></span></button><input type="hidden" name=""><button id="btns" type="button" class="col-xs-4 col-xs-offset-4 btn btn-success pull-right btn-number" data-type="plus" ><span class="glyphicon glyphicon-plus"></span></button></div></div>';
+    	$.each(data, function(index,value) {
+		  	var itemBox = '<div class="col-xs-2 itemsboxes" id="'+value.id+'"><div class="col-xs-12 wtSize"><div class="col-xs-4"><input type="text" class="form-control input-number" placeholder="Qty" value="'+value.Qty+'"></div><div class="col-xs-5"><input type="text" class="form-control" placeholder="Kg" value="'+value.Kg+'"></div><div class="col-xs-3"><i class="fa fa-laptop fa-3x" style="float:right"></i></div></div><div class="clearfix"></div><div class="col-xs-12"><div class="itemName">'+value.itemName+'</div></div><div class="clearfix"></div><div class="col-xs-12 wtSize"><div class="col-xs-6"><select class="form-control"><option>Small</option><option>Medium</option><option>Large</option><option>Ex Large</option></select></div><div class="col-xs-6"><div class="checkbox"><label><input type="checkbox" '+value.Delicate+'> Delicate</label></div></div></div><div class="clearfix"></div><div class="itemsboxBtns"><button type="button" class="col-xs-4 btn btn-default btn-Minus"  data-type="minus"><span class="glyphicon glyphicon-minus"></span></button><input type="hidden" name=""><button type="button" class="col-xs-4 col-xs-offset-4 btn btn-default pull-right btn-Plus" data-type="plus" ><span class="glyphicon glyphicon-plus"></span></button></div></div>';
 		    $("#"+container).append(itemBox);
-		  });
 		});
     }
     ItemsBoxes(livingRoomItems,'livingRoomItems');
-	$('[href="#pane2"]').click(function(){ItemsBoxes(bedRoomItems,'bedRoomItems');});
-	$('[href="#pane3"]').click(function(){ItemsBoxes(kitchenItems,'kitchenItems');});
-	$('[href="#pane4"]').click(function(){ItemsBoxes(storeRoomItems,'storeRoomItems');});
-	$('[href="#pane5"]').click(function(){ItemsBoxes(poojaRoomItems,'poojaRoomItems');});
-	$('[href="#pane6"]').click(function(){ItemsBoxes(vehicleItems,'vehicleItems');});
-	$('[href="#pane7"]').click(function(){ItemsBoxes(otherItems,'otherItems');});
+    ItemsBoxes(bedRoomItems,'bedRoomItems');
+    ItemsBoxes(kitchenItems,'kitchenItems');
+    ItemsBoxes(storeRoomItems,'storeRoomItems');
+    ItemsBoxes(poojaRoomItems,'poojaRoomItems');
+    ItemsBoxes(vehicleItems,'vehicleItems');
+    ItemsBoxes(otherItems,'otherItems');
+
+    $(".tab-content").on('click','.btn-Plus',function(){
+    	var qty = $(this).closest(".itemsboxes").find("[placeholder='Qty']").val();
+    	$(this).closest(".itemsboxes").find("[placeholder='Qty']").val(++qty);
+    });
+    $(".tab-content").on('click','.btn-Minus',function(){
+    	var qty = $(this).closest(".itemsboxes").find("[placeholder='Qty']").val();
+    	$(this).closest(".itemsboxes").find("[placeholder='Qty']").val(--qty);
+    	if(qty<=0)
+    		$(this).closest(".itemsboxes").find("[placeholder='Qty']").val("");
+    });
 });
