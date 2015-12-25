0c25bd4e469da368f8504363e092d7c925f7cd6d
ranjitbalu <ranjit.balu@gmail.com>
2015-12-25 21:12:01 +0530

    Changed Itemname size to Varchar50

--- database/packersandmovers.sql
+++ database/packersandmovers.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2015 at 08:01 PM
+-- Generation Time: Dec 25, 2015 at 04:40 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -23,125 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE IF NOT EXISTS `admin` (
-  `adminId` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  PRIMARY KEY (`adminId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`adminId`, `username`, `password`) VALUES
-(1, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bookingdateandtime`
---
-
-CREATE TABLE IF NOT EXISTS `bookingdateandtime` (
-  `bookingId` int(11) NOT NULL AUTO_INCREMENT,
-  `bookingTime` time NOT NULL,
-  `shipingDate` date NOT NULL,
-  `deliveryDate` date NOT NULL,
-  `currentAddressId` int(11) NOT NULL,
-  `destinationAddressId` int(11) NOT NULL,
-  `currentItemsId` varchar(25) NOT NULL,
-  `otherItemsId` varchar(25) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `bookingModifiedTime` time NOT NULL,
-  `sharingType` tinyint(1) NOT NULL,
-  `p/up` int(1) NOT NULL,
-  `currentStatus` tinyint(4) NOT NULL,
-  PRIMARY KEY (`bookingId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `bookingdateandtime`
---
-
-INSERT INTO `bookingdateandtime` (`bookingId`, `bookingTime`, `shipingDate`, `deliveryDate`, `currentAddressId`, `destinationAddressId`, `currentItemsId`, `otherItemsId`, `userId`, `bookingModifiedTime`, `sharingType`, `p/up`, `currentStatus`) VALUES
-(1, '06:35:00', '2015-12-16', '0000-00-00', 1, 1, '1,2,3,4,5,6,7,8', '1,2,3,4', 1, '00:00:00', 0, 0, 0),
-(2, '10:00:00', '2015-12-08', '0000-00-00', 2, 2, '9,10,11,12,13', '5,6,7,8,9', 1, '00:00:00', 0, 0, 0),
-(3, '06:30:00', '2015-12-30', '0000-00-00', 3, 3, '14,15,16', '10,11', 1, '00:00:00', 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `currentaddress`
---
-
-CREATE TABLE IF NOT EXISTS `currentaddress` (
-  `currentAddressId` int(11) NOT NULL AUTO_INCREMENT,
-  `currentAddress` varchar(25) NOT NULL,
-  `numberOfBedRooms` int(1) NOT NULL,
-  `houseType` varchar(25) NOT NULL,
-  `floorNumber` int(1) NOT NULL,
-  `elevator` tinyint(4) NOT NULL,
-  `packing` tinyint(4) NOT NULL,
-  `loading` tinyint(4) NOT NULL,
-  `userId` int(11) NOT NULL,
-  PRIMARY KEY (`currentAddressId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `currentaddress`
---
-
-INSERT INTO `currentaddress` (`currentAddressId`, `currentAddress`, `numberOfBedRooms`, `houseType`, `floorNumber`, `elevator`, `packing`, `loading`, `userId`) VALUES
-(1, 'nandyal', 2, 'Individual House', 2, 0, 1, 0, 1),
-(2, 'kurnool', 2, 'Individual House', 2, 0, 1, 1, 1),
-(3, 'bang', 2, 'Individual House', 3, 1, 1, 0, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `currentitems`
---
-
-CREATE TABLE IF NOT EXISTS `currentitems` (
-  `currentItemsId` int(11) NOT NULL AUTO_INCREMENT,
-  `itemId` int(11) NOT NULL,
-  `itemWeight` int(3) NOT NULL,
-  `itemType` varchar(25) NOT NULL,
-  `itemQuantity` int(3) NOT NULL,
-  `itemsize` varchar(25) NOT NULL,
-  `delicate` tinyint(4) NOT NULL,
-  PRIMARY KEY (`currentItemsId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
-
---
--- Dumping data for table `currentitems`
---
-
-INSERT INTO `currentitems` (`currentItemsId`, `itemId`, `itemWeight`, `itemType`, `itemQuantity`, `itemsize`, `delicate`) VALUES
-(1, 1, 12, '', 2, 'Medium', 0),
-(2, 2, 20, '', 1, 'Large', 0),
-(3, 3, 0, '', 2, 'Small', 0),
-(4, 4, 10, '', 1, 'Ex Large', 0),
-(5, 5, 13, '', 1, 'Small', 0),
-(6, 86, 50, '', 2, 'Medium', 0),
-(7, 92, 1400, '', 1, 'Small', 0),
-(8, 106, 150, '', 1, 'Small', 0),
-(9, 1, 12, '', 2, 'Small', 0),
-(10, 2, 40, '', 2, 'Small', 0),
-(11, 3, 0, '', 1, 'Small', 0),
-(12, 4, 20, '', 2, 'Small', 0),
-(13, 5, 26, '', 2, 'Small', 0),
-(14, 1, 12, '', 2, 'Small', 1),
-(15, 2, 40, '', 2, 'Small', 1),
-(16, 3, 0, '', 1, 'Small', 0);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `defaultitems`
 --
 
@@ -149,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `defaultitems` (
   `itemId` int(11) NOT NULL AUTO_INCREMENT,
   `roomType` int(11) NOT NULL,
   `itemType` varchar(11) NOT NULL,
-  `itemName` varchar(25) NOT NULL,
+  `itemName` varchar(50) NOT NULL,
   `weight` int(3) NOT NULL,
   `icons` varchar(255) NOT NULL,
   PRIMARY KEY (`itemId`)
@@ -163,8 +44,8 @@ INSERT INTO `defaultitems` (`itemId`, `roomType`, `itemType`, `itemName`, `weigh
 (1, 1, '0', 'Open Aquarium ', 6, 'open aquarium black.png'),
 (2, 1, '0', 'Center-Table', 20, 'Centre-Table.png'),
 (3, 1, '0', 'Chandelier', 0, 'Chandelier.png'),
-(4, 1, '0', 'Computer', 10, 'computer.png'),
-(5, 1, '0', 'Computer-Table', 13, 'computer table.png'),
+(4, 4, '0', 'Computer', 10, 'computer.png'),
+(5, 4, '0', 'Computer-Table', 13, 'computer table.png'),
 (6, 1, '0', 'Foldable-Chair', 4, 'foldable chair.png'),
 (7, 1, '0', 'Plastic-Chair', 3, 'Plastic-Chair.png'),
 (8, 1, '0', 'Non-Foldable-chair', 3, 'Non-Foldable-Chair.png'),
@@ -219,8 +100,8 @@ INSERT INTO `defaultitems` (`itemId`, `roomType`, `itemType`, `itemName`, `weigh
 (57, 3, '0', 'Gas Stove', 10, 'Gas stove.png'),
 (58, 3, '0', 'Gas-Stove big', 15, 'Gas-Stove big.png '),
 (59, 3, '0', 'Gas-Stove small', 10, 'Gas-Stove small.png'),
-(60, 3, '0', 'glass Dining table 4 chai', 70, 'Glass-Dining table 4 chairs.png'),
-(61, 3, '0', 'Glass-Dining table 6 chai', 85, 'glass Dining table 6 chairs.png'),
+(60, 3, '0', 'glass Dining table 4 chair', 70, 'Glass-Dining table 4 chairs.png'),
+(61, 3, '0', 'Glass-Dining table 6 chair', 85, 'glass Dining table 6 chairs.png'),
 (62, 3, '0', 'kitchen wear', 10, 'kitchen wear.png'),
 (63, 3, '0', 'Kitchen-Boxes ', 10, 'Kitchen-Boxes.png'),
 (64, 3, '0', 'Kitchen-Boxes ', 15, 'Kitchen-Boxes.png'),
@@ -244,201 +125,37 @@ INSERT INTO `defaultitems` (`itemId`, `roomType`, `itemType`, `itemName`, `weigh
 (82, 1, '0', 'office desk small', 8, 'office desk small.png'),
 (83, 1, '0', 'Storage Cabinet', 17, 'Storage Cabinet.png'),
 (84, 2, '0', 'Bean bag', 5, 'Bean bag.png'),
-(85, 6, '0', 'Bench-gym', 10, 'Bench-gym.png'),
-(86, 7, '0', 'bicycle', 25, 'bicycle.png'),
+(85, 7, '0', 'Bench-gym', 10, 'Bench-gym.png'),
+(86, 8, '0', 'bicycle', 25, 'bicycle.png'),
 (87, 2, '0', 'Books-boxes', 50, 'Books-boxes.png'),
 (88, 2, '0', 'Book-shelf', 20, 'Book-shelf.png'),
-(89, 4, '0', 'box under 50kg to 70kg', 60, 'box under 50kg to 70kg.png'),
-(90, 4, '0', 'box under 50kg', 50, 'box under 50kg.png'),
-(91, 7, '0', 'car(big)', 1800, 'car(big).png'),
-(92, 7, '0', 'car(small)', 1400, 'car(small).png'),
+(89, 5, '0', 'box under 50kg to 70kg', 60, 'box under 50kg to 70kg.png'),
+(90, 5, '0', 'box under 50kg', 50, 'box under 50kg.png'),
+(91, 8, '0', 'car(big)', 1800, 'car(big).png'),
+(92, 8, '0', 'car(small)', 1400, 'car(small).png'),
 (93, 2, '0', 'Ceiling Fans ', 2, 'Ceiling Fans .png'),
 (94, 2, '0', 'Chest of Drawers', 8, 'Chest of Drawers.png'),
 (95, 2, '0', 'Cloth Stand', 5, 'Cloth Stand.png'),
-(96, 6, '0', 'Cross-Trainer', 25, 'Cross-Trainer.png'),
-(97, 6, '0', 'dumbbell above 50kg black', 100, 'dumbbell above 50kg black.png'),
-(98, 6, '0', 'dumbbell below 50kg black', 60, 'dumbbell below 50kg black.png'),
-(99, 6, '0', 'Exercise-cycle', 25, 'Exercise-cycle.png'),
+(96, 7, '0', 'Cross-Trainer', 25, 'Cross-Trainer.png'),
+(97, 7, '0', 'dumbbell above 50kg black', 100, 'dumbbell above 50kg black.png'),
+(98, 7, '0', 'dumbbell below 50kg black', 60, 'dumbbell below 50kg black.png'),
+(99, 7, '0', 'Exercise-cycle', 25, 'Exercise-cycle.png'),
 (100, 1, '0', 'Flower-pot', 4, 'Flower-pot.png'),
 (101, 2, '0', 'Geyser', 45, 'Geyser.png'),
-(102, 6, '0', 'Gym-all-in-one', 100, 'Gym-all-in-one.png'),
+(102, 7, '0', 'Gym-all-in-one', 100, 'Gym-all-in-one.png'),
 (103, 2, '0', 'Iron Rack', 25, 'Iron Rack.png'),
 (104, 2, '0', 'Mirror', 15, 'Mirror.png'),
 (105, 1, '0', 'Misc-boxes', 25, 'Misc-boxes.png'),
-(106, 7, '0', 'motar cycle', 150, 'motar cycle.png'),
+(106, 8, '0', 'motar cycle', 150, 'motar cycle.png'),
 (107, 1, '0', 'Piano', 50, 'Piano.png'),
 (108, 3, '0', 'Plastic-drum', 5, 'Plastic-drum.png'),
-(109, 5, '0', 'Pooja-temple', 10, 'Pooja-temple.png'),
+(109, 6, '0', 'Pooja-temple', 10, 'Pooja-temple.png'),
 (110, 1, '0', 'Sewing-machine', 13, 'Sewing-machine.png'),
 (111, 1, '0', 'Shoe Rack', 20, 'Shoe Rack.png'),
 (112, 1, '0', 'Swing-chair', 10, 'Swing-chair.png'),
-(113, 6, '0', 'Treadmill', 35, 'Treadmill.png'),
+(113, 7, '0', 'Treadmill', 35, 'Treadmill.png'),
 (114, 1, '0', 'Vacuum-cleaner', 5, 'Vacuum-cleaner.png'),
 (115, 1, '0', 'Washing-machine', 25, 'Washing-machine.png');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `destinationaddress`
---
-
-CREATE TABLE IF NOT EXISTS `destinationaddress` (
-  `destinationAddressId` int(11) NOT NULL AUTO_INCREMENT,
-  `destinationAddress` varchar(55) NOT NULL,
-  `numberOfBedRooms` int(1) NOT NULL,
-  `destinationHouseType` varchar(25) NOT NULL,
-  `floorNumber` int(1) NOT NULL,
-  `elevator` tinyint(4) NOT NULL,
-  `unpacking` int(11) NOT NULL,
-  `unloading` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  PRIMARY KEY (`destinationAddressId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `destinationaddress`
---
-
-INSERT INTO `destinationaddress` (`destinationAddressId`, `destinationAddress`, `numberOfBedRooms`, `destinationHouseType`, `floorNumber`, `elevator`, `unpacking`, `unloading`, `userId`) VALUES
-(1, 'nalgonda', 0, 'Villa', 5, 1, 1, 0, 1),
-(2, 'hyderabad', 0, 'Villa', 11, 1, 1, 1, 1),
-(3, 'delhi', 0, 'Individual House', 3, 0, 1, 0, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `feedback`
---
-
-CREATE TABLE IF NOT EXISTS `feedback` (
-  `feedbackId` int(11) NOT NULL AUTO_INCREMENT,
-  `userId` int(11) NOT NULL,
-  `feedback` varchar(255) NOT NULL,
-  PRIMARY KEY (`feedbackId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `keycodes`
---
-
-CREATE TABLE IF NOT EXISTS `keycodes` (
-  `keyCodesId` int(11) NOT NULL AUTO_INCREMENT,
-  `city` varchar(25) NOT NULL,
-  `area` varchar(25) NOT NULL,
-  `pinCode` int(1) NOT NULL,
-  PRIMARY KEY (`keyCodesId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `otheritems`
---
-
-CREATE TABLE IF NOT EXISTS `otheritems` (
-  `otherItemsId` int(11) NOT NULL AUTO_INCREMENT,
-  `otherItemName` varchar(25) NOT NULL,
-  `otherItemQuantity` int(11) NOT NULL,
-  `otherItemWeight` varchar(25) NOT NULL,
-  `otherItemDelicate` tinyint(4) NOT NULL,
-  `otherItemType` varchar(22) NOT NULL,
-  `otherItemSize` varchar(22) NOT NULL,
-  PRIMARY KEY (`otherItemsId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
-
---
--- Dumping data for table `otheritems`
---
-
-INSERT INTO `otheritems` (`otherItemsId`, `otherItemName`, `otherItemQuantity`, `otherItemWeight`, `otherItemDelicate`, `otherItemType`, `otherItemSize`) VALUES
-(1, 'aa', 1, '23', 1, 'wood', 'ex large'),
-(2, 'bb', 2, '32', 0, 'Glass', 'large'),
-(3, 'ccc', 3, '43', 1, 'Plastic', 'medium'),
-(4, 'dd', 4, '54', 0, 'Iron', 'small'),
-(5, 'ab', 1, '20', 0, 'iron', '="small"'),
-(6, 'ac', 1, '24', 1, 'Glass', 'large'),
-(7, 'ad', 2, '', 1, 'Wood', 'Ex large'),
-(8, 'ae', 1, '', 0, 'Plastic', 'medium'),
-(9, 'af', 3, '23', 0, 'Glass', 'small'),
-(10, 'aaaa', 1, '1', 1, 'iron', '="small"'),
-(11, 'bbbb', 2, '2', 0, 'Iron', 'small');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `roomtype`
---
-
-CREATE TABLE IF NOT EXISTS `roomtype` (
-  `roomTypeId` int(11) NOT NULL AUTO_INCREMENT,
-  `roomName` varchar(25) NOT NULL,
-  PRIMARY KEY (`roomTypeId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
-
---
--- Dumping data for table `roomtype`
---
-
-INSERT INTO `roomtype` (`roomTypeId`, `roomName`) VALUES
-(1, 'Living Room'),
-(2, 'Bed Room'),
-(3, 'Kitchen'),
-(4, 'Store Room'),
-(5, 'Pooja Room'),
-(6, 'Gym'),
-(7, 'Vehicles'),
-(8, 'Others');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `userName` varchar(25) NOT NULL,
-  `email` varchar(25) NOT NULL,
-  `primaryContact` bigint(10) NOT NULL,
-  `secondaryContact` bigint(10) NOT NULL,
-  `points` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  `userType` int(1) NOT NULL,
-  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userId`, `userName`, `email`, `primaryContact`, `secondaryContact`, `points`, `status`, `userType`) VALUES
-(1, '', 'sss@gmail.com', 9656569652, 0, 0, 0, 0),
-(2, '', 'sss@gmail.com', 9656569652, 0, 0, 0, 0),
-(3, '', 'sss@gmail.com', 9656569652, 0, 0, 0, 0),
-(4, '', 'msd@gmail.com', 9656569658, 0, 0, 0, 0),
-(5, '', 'msd@gmail.com', 9656569658, 0, 0, 0, 0),
-(6, '', 'msd1@gmail.com', 9656569658, 0, 0, 0, 0),
-(7, '', 'msd12@gmail.com', 9656569658, 0, 0, 0, 0),
-(8, '', 'mssd12@gmail.com', 9656569658, 0, 0, 0, 0),
-(9, '', 'mssd@gmail.com', 9656569658, 0, 0, 0, 0),
-(10, '', 'mssd123@gmail.com', 9656569658, 0, 0, 0, 0),
-(11, '', 'sudha@gmail.com', 9656569658, 0, 0, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vendordetailsandprice`
---
-
-CREATE TABLE IF NOT EXISTS `vendordetailsandprice` (
-  `vendorId` int(11) NOT NULL AUTO_INCREMENT,
-  `vendorName` varchar(25) NOT NULL,
-  `price` int(6) NOT NULL,
-  `sharingType` tinyint(4) NOT NULL,
-  PRIMARY KEY (`vendorId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
