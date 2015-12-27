4af384dffeb113871ceff9a7e172a20b58cdaa32
hariprakashmuvva <harimuvva150478@gmail.com>
2015-12-27 07:45:10 +0530

    scrolltop when we are moving into next section

--- frontend/js/items.js
+++ frontend/js/items.js
@@ -9,12 +9,31 @@ $(document).ready(function(){
 		$(".roomsDropdown").find("li").removeClass('active');
 	});
 
+    var progressArr = [{'count':'one','width':15},{'count':'two','width':15},{'count':'three','width':15},{'count':'four','width':10},{'count':'five','width':10},{'count':'six','width':10},{'count':'seven','width':5},{'count':'eight','width':5},{'count':'nine','width':5},{'count':'ten','width':10}];
+    
+    function progressBar(tabno){
+        var pbCount=0;
+        jQuery.each(progressArr, function(index,value){
+            if(value.count ==  tabno){
+                pbCount = value.width;
+                console.log(pbCount);
+            }
+        });
 
-    var progressArr = [15,15,15,10,10,10,5,5,5,15];
+        for(var i=0; i<5; i++){
+            var x = progressArr[i].width;
+            j=i+1;
+            $(".progress > div:nth-child("+j+")").css('width',x+"%");
+        }
+    }
+    progressBar('six');
+
 	$(".list-group-item").click(function(){
             $(".list-group-item").removeClass('active');
             $(this).addClass('active');
-            
+            $(document).scrollTop(0);
+            var roomtype = $(this).data('roomtype');
+            console.log(roomtype);
     });
 
     $(".othersData").on('click','.addOtherrow',function(){
@@ -32,20 +51,11 @@ $(document).ready(function(){
         }
     });
 
-    $(".tab-content, .othersData").on('keyup','.Kilograms', function(){
-        kilos = 0;
-          $( ".Kilograms" ).each(function( index, element ) {
-                if($(this).val() != "" && $(this).val() != 0){
-                       kilos  = kilos + parseInt($(this).val());
-                }
-          });
-          $(".totalkgs").val(kilos).trigger('click');
-
-    });
     $(".navbtns").click(function(){
         $(".list-group").find(".list-group-item").removeClass("active");
         var tab = $(this).data("roomtype");
         $(".list-group").find('.'+tab).addClass("active");
+        $(document).scrollTop(0);
     });
 
     $(".totalkgs").click(function(){
@@ -63,6 +73,7 @@ $(document).ready(function(){
      if(key == 13)  // the enter key code
       {
         $(this).blur();
+        $(this).closest('.tab-content').find('.Kilograms').trigger('click');
         return false;  
       }
     });
@@ -88,7 +99,7 @@ $(document).ready(function(){
                 var vehicleItems = data.vehicles;
                 function ItemsBoxes(data, container){
                     $.each(data, function(index,value) {
-                        var itemBox = '<div class="col-xs-2 itemsboxes" id="'+value.itemId+'"><div class="col-xs-12 wtSize"><div class="col-xs-4"><input type="text" class="form-control quantity" name="itemsquantity[]" placeholder="Qty" value=""></div><div class="col-xs-5"><input type="hidden" class="form-control" name="" placeholder="aKg" value="'+value.weight+'"><input type="text" class="form-control Kilograms" name="itemweight[]" placeholder="Kg" value=""></div><div class="col-xs-3"><img src="./icon/'+value.roomName+'/png black/'+value.icons+'"  style="float:right"></img></div></div><div class="clearfix"></div><div class="boxOfItemName"><div class="itemName">'+value.itemName+'</div></div><div class="clearfix"></div><div class="col-xs-12 wtSize2"><div class="col-xs-6"><select class="form-control" name="itemsize[]" ><option>Small</option><option>Medium</option><option>Large</option><option>Ex Large</option></select></div><div class="col-xs-6"><div class="checkbox"><label><input type="checkbox" name="delicate[]" value="'+value.itemId+'" > Delicate</label></div></div></div><div class="clearfix"></div><div class="itemsboxBtns"><button type="button" class="col-xs-4 btn btn-default btn-Minus"  data-type="minus"><span class="glyphicon glyphicon-minus"></span></button><input type="hidden" name="itemid[]" value="'+value.itemId+'"><button type="button" class="col-xs-4 col-xs-offset-4 btn btn-default pull-right btn-Plus" data-type="plus" ><span class="glyphicon glyphicon-plus"></span></button></div></div>';
+                        var itemBox = '<div class="col-xs-2 itemsboxes" id="'+value.itemId+'"><div class="col-xs-12 wtSize"><div class="col-xs-4"><input type="text" class="form-control quantity" name="itemsquantity[]" placeholder="Qty" value=""></div><div class="col-xs-5"><input type="hidden" class="form-control" name="" placeholder="aKg" value="'+value.weight+'"><input type="text" class="form-control Kilograms" disabled name="itemweight[]" placeholder="Kg" value=""></div><div class="col-xs-3"><img src="./icon/'+value.roomName+'/png black/'+value.icons+'"  style="float:right"></img></div></div><div class="clearfix"></div><div class="boxOfItemName"><div class="itemName">'+value.itemName+'</div></div><div class="clearfix"></div><div class="col-xs-12 wtSize2"><div class="col-xs-6"><select class="form-control" disabled name="itemsize[]" ><option>Small</option><option>Medium</option><option>Large</option><option>Ex Large</option></select></div><div class="col-xs-6"><div class="checkbox" disabled><label><input class="delicatecheckbox" type="checkbox" disabled name="delicate[]" value="'+value.itemId+'" > Delicate</label></div></div></div><div class="clearfix"></div><div class="itemsboxBtns"><button type="button" class="col-xs-4 btn btn-default btn-Minus"  data-type="minus"><span class="glyphicon glyphicon-minus"></span></button><input type="hidden" name="itemid[]" value="'+value.itemId+'"><button type="button" class="col-xs-4 col-xs-offset-4 btn btn-default pull-right btn-Plus" data-type="plus" ><span class="glyphicon glyphicon-plus"></span></button></div></div>';
                         $("#"+container).append(itemBox);
                     });
                 }
@@ -109,100 +120,121 @@ $(document).ready(function(){
     });
 
     $(".tab-content").on('click','.btn-Plus',function(){
-var qty = $(this).closest(".itemsboxes").find("[placeholder='Qty']").val();
-
-        //var wt = $(this).closest(".itemsboxes").find("[placeholder='aKg']").val();
-        //wt =12;
-       // alert(wt);
-        
-        //var awt = $(this).closest(".itemsboxes").find("[placeholder='aKg']").val();
-        $(this).closest(".itemsboxes").find("[placeholder='Qty']").val(++qty);
+        var qty = $(this).closest(".itemsboxes").find(".quantity").val();
+        $(this).closest(".itemsboxes").find(".quantity").val(++qty);
         if(qty==1)
         {
            var wt = $(this).closest(".itemsboxes").find("[placeholder='aKg']").val();
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt*qty);
+            $(this).closest(".itemsboxes").find(".Kilograms").val(wt*qty);
+            $(this).closest(".itemsboxes").find(".quantity, .Kilograms, select, .delicatecheckbox, .checkbox").prop('disabled', false);
         }
         else if (qty==2)
         {
-            wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt*qty);
-        }else{
-            wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-            var count = qty;
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt/--count);
-            var wt1 = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt1*qty);
+            wt = $(this).closest(".itemsboxes").find(".Kilograms").val();
+            $(this).closest(".itemsboxes").find(".Kilograms").val(wt*qty);
+            $(this).closest(".itemsboxes").find(".Kilograms").prop('disabled', true);
         }
-
+        else{
+            wt = $(this).closest(".itemsboxes").find(".Kilograms").val();
+            var count = qty;
+            $(this).closest(".itemsboxes").find(".Kilograms").val(wt/--count);
+            var wt1 = $(this).closest(".itemsboxes").find(".Kilograms").val();
+            $(this).closest(".itemsboxes").find(".Kilograms").val(wt1*qty);
+            $(this).closest(".itemsboxes").find(".Kilograms").prop('disabled', true);
+        }
         $(this).closest(".itemsboxes").find(".quantity").trigger('click');
+        $(this).closest(".itemsboxes").find(".Kilograms").trigger('change');
+        $(this).closest(".itemsboxes").addClass('filledClass');
+    });
 
-        $(this).closest(".itemsboxes").find(".Kilograms").trigger('click');
+    $(".tab-content, .othersData").on('change','.Kilograms', function(){
+        console.log("Hari");
+        kilos = 0;
+          $( ".Kilograms" ).each(function( index, element ) {
+                if($(this).val() != "" && $(this).val() != 0){
+                       kilos  = kilos + parseInt($(this).val());
+                }
+          });
+          $(".totalkgs").val(kilos).trigger('click');
     });
 
     $(".tab-content").on('click','.btn-Minus',function(){
-    	var qty = $(this).closest(".itemsboxes").find("[placeholder='Qty']").val();
+    	var qty = $(this).closest(".itemsboxes").find(".quantity").val();
         var wt = $(this).closest(".itemsboxes").find("[placeholder='aKg']").val();
-        //var wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-    	$(this).closest(".itemsboxes").find("[placeholder='Qty']").val(--qty);
+    	$(this).closest(".itemsboxes").find(".quantity").val(--qty);
         if(qty==1)
         {
             if(wt==""){
                 wt = $(this).closest(".itemsboxes").find("[placeholder='aKg']").val();
             }
             else{
-                wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-                $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt/2);
-                wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
+                wt = $(this).closest(".itemsboxes").find(".Kilograms").val();
+                $(this).closest(".itemsboxes").find(".Kilograms").val(wt/2);
+                wt = $(this).closest(".itemsboxes").find(".Kilograms").val();
             }
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt*qty);
+            $(this).closest(".itemsboxes").find(".Kilograms").val(wt*qty);
+            $(this).closest(".itemsboxes").find(".quantity, .Kilograms, select, .delicatecheckbox, .checkbox").prop('disabled', false);
         }
         else {
-            wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
+            wt = $(this).closest(".itemsboxes").find(".Kilograms").val();
             var count = qty;
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt/++count);
-            var wt1 = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt1*qty);
+            $(this).closest(".itemsboxes").find(".Kilograms").val(wt/++count);
+            var wt1 = $(this).closest(".itemsboxes").find(".Kilograms").val();
+            $(this).closest(".itemsboxes").find(".Kilograms").val(wt1*qty);
+            $(this).closest(".itemsboxes").find(".Kilograms").prop('disabled', true);
         }
-        //$(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt*qty);
-        $(this).closest(".itemsboxes").find(".Kilograms").trigger('click');
     	if(qty<=0){
-    		$(this).closest(".itemsboxes").find("[placeholder='Qty']").val("");
+    		$(this).closest(".itemsboxes").find(".quantity").val("");
             $(this).closest(".itemsboxes").find(".quantity").trigger('click');
-            $(this).closest(".itemsboxes").find("[placeholder='Kg']").val("");
-            $(this).closest(".itemsboxes").find(".Kilograms").trigger('click');
+            $(this).closest(".itemsboxes").find('.Kilograms').val("");
+            $(this).closest(".itemsboxes").removeClass('filledClass');
+            $(this).closest(".itemsboxes").find(".Kilograms, select, .delicatecheckbox, .checkbox").prop('disabled', true);
         }
-
-
+        $(this).closest('.tab-content').find('.Kilograms').trigger('change');
     });
 	
-	   $(".tab-content").on('focus','.quantity', function(){
-        qtyfocus = $(this).closest(".itemsboxes").find("[placeholder='Qty']").val();
+	$(".tab-content").on('focus','.quantity', function(){
+        qtyfocus = $(this).closest(".itemsboxes").find(".quantity").val();
     });
 	
 	$(".tab-content").on('change','.quantity', function(){
-        var qty = $(this).closest(".itemsboxes").find("[placeholder='Qty']").val();
-        var wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
+        var qty = $(this).closest(".itemsboxes").find(".quantity").val();
+        var wt = $(this).closest(".itemsboxes").find(".Kilograms").val();
         if(qty > 0){
             $(this).closest(".itemsboxes").addClass('filledClass');
             if(wt!=""  && wt!=NaN){
-                wt = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-                $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt/qtyfocus);
-                wt1 = $(this).closest(".itemsboxes").find("[placeholder='Kg']").val();
-                $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt1*qty);
+                wt = $(this).closest(".itemsboxes").find(".Kilograms").val();
+                $(this).closest(".itemsboxes").find(".Kilograms").val(wt/qtyfocus);
+                wt1 = $(this).closest(".itemsboxes").find(".Kilograms").val();
+                $(this).closest(".itemsboxes").find(".Kilograms").val(wt1*qty);
             }else{
                 wt = $(this).closest(".itemsboxes").find("[placeholder='aKg']").val();
-                $(this).closest(".itemsboxes").find("[placeholder='Kg']").val(wt*qty);
+                $(this).closest(".itemsboxes").find(".Kilograms").val(wt*qty);
+            }
+            if(qty>=1){
+                $(this).closest(".itemsboxes").find(".Kilograms, select, .delicatecheckbox, .checkbox").prop('disabled', false);
+            }
+            if(qty==1){
+                $(this).closest(".itemsboxes").find(".Kilograms").prop('disabled', true);
+            }
+            else
+            {
+                $(this).closest(".itemsboxes").find(".Kilograms").prop('disabled', true);
             }
 
-        }else{
-            $(this).closest(".itemsboxes").removeClass('filledClass');
+        }
+        else{
             if(qty<=0){
-                $(this).closest(".itemsboxes").find("[placeholder='Qty']").val("");
+                $(this).closest(".itemsboxes").find(".quantity").val("");
                 $(this).closest(".itemsboxes").find(".quantity").trigger('click');
-                $(this).closest(".itemsboxes").find("[placeholder='Kg']").val("");
-                $(this).closest(".itemsboxes").find(".Kilograms").trigger('click');
+                $(this).closest(".itemsboxes").find(".Kilograms").val("");
+                $(this).closest(".itemsboxes").find(".Kilograms").trigger('change');
+                $(this).closest(".itemsboxes").removeClass('filledClass');
+                $(this).closest(".itemsboxes").find(".Kilograms, select, .delicatecheckbox, .checkbox").prop('disabled', true);
             }
         }
+
+                $(this).closest(".itemsboxes").find(".Kilograms").trigger('change');
     });
 	
 /*
